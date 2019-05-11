bad63a37a391734c2c43a6e2136d4403ec369a88
zh <361776225@qq.com>
2019-05-11 18:59:26 +0800

    init demo server

---
+++ demo-server/src/main/java/com/zh/demo/DemoServerApplication.java
@@ -0,0 +1,21 @@
+package com.zh.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServerApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("test ok..");
+    }
+}
